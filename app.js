281b679a12aa75a0cde68a8ba3a45ad1dc533bf5
mikan2000 <lsmxinjing@gmail.com>
2014-03-23 16:00:06 +0800

    update msg

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 var express = require('express');
 var webot = require('weixin-robot');
+var googleapis = require('./lib/googleapis.js');
 
 var app = express();
-
 // 指定回复消息
-webot.set('hi', '你好');
-
 webot.set('subscribe', {
   pattern: function(info) {
     return info.is('event') && info.param.event === 'subscribe';
   },
   handler: function(info) {
-    return '欢迎订阅微信机器人';
+    return '欢迎订阅短网址小助手，发送一个长网址给我，会返回一个短网址哦~如果是淘宝网址，请将返回的短网址复制到浏览器中打开。';
+  }
+});
+webot.set('shorturl', {
+  pattern: '^((http|https)://)?[\\\S]+\\\.[\\\S]+$',
+  handler: function(info, next) {
+    var msg = "fanhui";
+    googleapis
+    .discover('urlshortener', 'v1')
+    .execute(function(err, client) {
+      client.urlshortener.url
+          .insert({ longUrl: info.text })
+          .execute(function(err, result) {
+            if (err) {
+              msg = err;
+              return next();
+            } else {
+              console.log('Result: ', result.id);
+              msg = result.id;
+              return next(null, msg);
+            }
+          });
+    });
   }
 });
 
 // 接管消息请求
 webot.watch(app, { token: 'poppy', path: '/wechat' });
-
-// 如果需要多个实例（即为多个微信账号提供不同回复）：
-// var webot2 = new webot.Webot();
-// webot2.set({
-//   '/hi/i': 'Hello',
-//   '/who (are|r) (you|u)/i': 'I\'m a robot.'
-// });
-// webot2.watch(app, {
-//   token: 'token2',
-//   path: '/wechat_en', // 这个path不能为之前已经监听过的path的子目录
-// });
-
-// 启动 Web 服务
-// 微信后台只允许 80 端口
 app.listen(80);
